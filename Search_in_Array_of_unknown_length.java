fc6a91446073b33b529b7086756e543b2f0a6e40
Aarav Bodla <147215351+aaravbodla@users.noreply.github.com>
2025-06-02 12:49:51 +0530

    Create Search_in_Array_of_unknown_length.java

---
+++ Search_in_Array_of_unknown_length.java
@@ -0,0 +1,29 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : none
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        int low = 0, high = 1;
+
+        while(reader.get(high) < target){ 
+            low = high ;
+            high = high * 2;
+        }
+
+        while(low <= high){ 
+            int mid = low + (high - low)/2;
+            if(reader.get(mid) == target) return mid;
+            if(reader.get(mid) > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+      
+        return -1;
+    }
+}
